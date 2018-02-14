13a9248def9a3a6407df9754c886145abca6b50f
phytertek <ryan.phytertek@gmail.com>
2018-02-14 14:54:12 -0500

    add airtable queries and replace /account route response to query results

---
+++ airtable.js
@@ -0,0 +1,46 @@
+const Airtable = require('airtable');
+const apiKey = process.env.AIRTABLE_API_KEY || 'keysRGO1v32tbkj2X';
+const root = process.env.AIRTABLE_ROOT || 'appUqpw2k7cfrkkC8';
+
+const Student = new Airtable({ apiKey }).base(root)('Students');
+const Test_Result = new Airtable({ apiKey }).base(root)('Test Results');
+
+const queries = {
+  getStudent: async username => {
+    try {
+      const query = Student.select({
+        maxRecords: 1,
+        filterByFormula: `{Github Username} = "${username}"`
+      });
+      const results = await query.firstPage();
+      return results[0];
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+  getStudentWithTestResults: async username => {
+    try {
+      const studentQuery = Student.select({
+        maxRecords: 1,
+        filterByFormula: `{Github Username} = "${username}"`
+      });
+      const studentResult = await studentQuery.firstPage();
+      // NOTE: This query on the Test Result table will only return 100 results due
+      // to airtable's pagination -- to be fully production ready we would need to
+      // account for this and re-query for the next pages if the results exceeded
+      // 100 records
+      const testResultsQuery = Test_Result.select({
+        filterByFormula: `{Student Record ID} = "${studentResult[0].id}"`
+      });
+      const testResults = await testResultsQuery.firstPage();
+      const trData = testResults.map(tr => tr.fields);
+      const sData = studentResult[0].fields;
+      sData['Test Results'] = trData;
+      return sData;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+module.exports = queries;
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const GitHubStrategy = require('passport-github2');
 const cors = require('cors');
 
+const airtable = require('./airtable');
+
 passport.serializeUser((user, done) => {
   done(null, user);
 });
@@ -13,35 +15,40 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "https://eviluator-auth.herokuapp.com/auth/github/callback"
-},
-  (accessToken, refreshToken, profile, done) => {
-    process.nextTick(function () {
-      // typically we want to associate GitHub account with a user record here
-      return done(null, profile);
-    });
-  }
-));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: 'https://eviluator-auth.herokuapp.com/auth/github/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(function() {
+        // typically we want to associate GitHub account with a user record here
+        return done(null, profile);
+      });
+    }
+  )
+);
 
 const server = express();
 
-server.use(cors({
-  allowedHeaders: ['X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'],
-  origin: ['http://localhost:3000', 'https://eviluator-client.herokuapp.com'],
-  credentials: true
-}));
-server.use(session({ secret: 'wowsosecret', resave: false, saveUninitialized: false }));
+server.use(
+  cors({
+    allowedHeaders: [
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+    ],
+    origin: ['http://localhost:3000', 'https://eviluator-client.herokuapp.com'],
+    credentials: true
+  })
+);
+server.use(
+  session({ secret: 'wowsosecret', resave: false, saveUninitialized: false })
+);
 server.use(passport.initialize());
 server.use(passport.session());
-server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-
-
-
-
 
 //update this right here is the problem
 // const checkAuthentication = (req, res, next) => {
@@ -57,15 +64,28 @@ server.get('/', (req, res) => {
   res.send('<h1>hi</h1>');
 });
 
-server.get('/auth/github', passport.authenticate('github', { scope: ['user'] }));
+server.get(
+  '/auth/github',
+  passport.authenticate('github', { scope: ['user'] })
+);
 
-server.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('https://eviluator-client.herokuapp.com/');
-});
+server.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('https://eviluator-client.herokuapp.com/');
+  }
+);
 
-server.get('/account', (req, res) => {
-  if (req.isAuthenticated()) return res.json({user: req.user});
-  res.json({user: null});
+server.get('/account', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      const username = req.user.username;
+      const user = await airtable.getStudentWithTestResults(username);
+      return res.json({ user });
+    }
+    res.json({ user: null });
+  } catch (error) {}
 });
 
 server.get('/logout', (req, res) => {
